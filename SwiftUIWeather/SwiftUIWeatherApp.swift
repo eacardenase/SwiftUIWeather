4971f4d704f56e0f75968b3b6476fe70f41898ce
Edwin Cardenas <ecardenas@lwolf.com>
2026-04-02 21:26:42 -0500

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Edwin Cardenas on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
